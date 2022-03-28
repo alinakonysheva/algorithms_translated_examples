7b6b33e0080ce9e3396ebd5821efa2c2bff86d10
Alina Konysheva <konysheva.alina@gmail.com>
2022-03-28 11:19:36 +0200

    hash

---
+++ hash_function.py
@@ -0,0 +1,21 @@
+# Determining the number of distinct substrings using a hash function.
+# Let a string S of length N be given, consisting only of small Latin letters.
+# Required to find the number of different substrings in this string.
+
+import hashlib
+
+
+def count_substrings(s):
+    # future substrings will be stored in the set to avoid duplication
+    h_subs = set()
+    # iterate over all possible segments of the string
+    for i in range(len(s)):
+        for j in range(1, len(s) + 1):
+            if s[i:j]:
+                h_subs.add(hashlib.sha1(s[i:j].encode('utf-8')).hexdigest())
+    # we return the value minus one, because the whole string also got into the set with a unique element
+    return len(h_subs) - 1
+
+
+s1 = input(f'Please enter a string consisting of Latin letters:   ')
+print(f'Number of substrings in your string: {count_substrings(s1)}')
